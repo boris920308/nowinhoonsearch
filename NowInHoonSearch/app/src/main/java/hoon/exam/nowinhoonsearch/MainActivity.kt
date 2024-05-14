5e8895c96b96bacae32993b45a16f55ae35ec227
boris920308 <jhoonj0308@gmail.com>
2024-05-14 18:09:28 +0900

    [feat] bottom navigation 추가

--- NowInHoonSearch/app/src/main/java/hoon/exam/nowinhoonsearch/MainActivity.kt
+++ NowInHoonSearch/app/src/main/java/hoon/exam/nowinhoonsearch/MainActivity.kt
@@ -1,16 +1,60 @@
 package hoon.exam.nowinhoonsearch
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.NavHost
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import hoon.exam.nowinhoonsearch.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    private lateinit var bottomNavigation: BottomNavigationView
+    private lateinit var navHostFragment: NavHostFragment
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        initNavigation()
+    }
+
+    private fun initNavigation() {
+        bottomNavigation = binding.bottomNav
+        navHostFragment = supportFragmentManager.findFragmentById(R.id.frag_container) as NavHostFragment
+        navController =navHostFragment.navController
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+
+        bottomNavigation.setupWithNavController(navController)
+        bottomNavigation.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.homeFragment -> {
+                    Log.d("MainActivity", "onNavigationItemSelected, item = homeFragment")
+                }
+
+                R.id.favoriteFragment -> {
+                    Log.d("MainActivity", "onNavigationItemSelected, item = favoriteFragment")
+                }
+
+                R.id.myPageFragment -> {
+                    Log.d("MainActivity", "onNavigationItemSelected, item = myPageFragment")
+                }
+            }
+            false
+        }
+
+
+
+
     }
 }
